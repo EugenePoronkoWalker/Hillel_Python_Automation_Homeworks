fdd04c8e887cee1deba9fa3f29b57e32c3d9e0c0
Yevhen Poronko <eugenewalkerporonko@gmail.com>
2023-03-18 19:05:37 +0200

    first homework

---
+++ homework_1.py
@@ -0,0 +1,78 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter, end= '')
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+print(banana)
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
+
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+apple_trees = 4
+pear_trees = apple_trees + 5
+plum_trees = apple_trees - 2
+total_trees = apple_trees + pear_trees + plum_trees
+print(total_trees)
+
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+in_the_morning = 5
+in_the_afternoon = in_the_morning - 10
+in_the_evening = in_the_afternoon + 4
+print(in_the_evening)
+
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+all_boys = 24
+all_girls = 24 / 2
+boys_on_a_sick_leave = 1
+girls_absent = 2
+kids_today = (all_boys + all_girls) - (boys_on_a_sick_leave + girls_absent)
+print(int(kids_today))
+
+
+# task 10
+"""
+Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+а третя - як половина вартості першої та другої разом.
+Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+"""
+first_book = 8
+second_book = 8 * 2
+third_book = (first_book + second_book) / 2
+all_books = first_book + second_book + third_book
+print(all_books)
